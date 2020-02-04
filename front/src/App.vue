7c3152074771d4476cf7dd83edb6e56d7505068e
Oizys18 <oizys18@gmail.com>
2020-02-04 15:30:14 +0900

    Add v-app into App.vue chanu Add vuetify

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
+    <v-app>
     <AppBar />
     <router-view />
+    </v-app>
   </div>
 </template>
 <script>
@@ -21,9 +23,9 @@ export default {
 #app {
   font-family: 'Noto Sans KR', sans-serif;
   /* font-family: "Avenir", Helvetica, Arial, sans-serif; */
-  -webkit-font-smoothing: antialiased;
+  /* -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: center; */
   /* color: #2c3e50; */
 }
 </style>
