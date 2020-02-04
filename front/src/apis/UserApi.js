43aa0a058b5b0dcb7b1e4d6cf4ca3699b8ee9f09
신채민 <chaem1996@gmail.com>
2020-02-04 11:30:28 +0900

    Mod UserApi.js chaemin login, join 기능

--- front/src/apis/UserApi.js
+++ front/src/apis/UserApi.js
@@ -5,32 +5,44 @@ import axios from 'axios'
 
 
 
-const requestLogin = (loginID, loginPW,callback,errorCallback) => {// eslint-disable-line no-unused-vars
-    //백앤드와 로그인 통신하는 부분
-    let loginData = new Map();
-    loginData.set('email', loginID);
-    loginData.set('password', loginPW);
-    return axios.post('http://localhost:8081/user/login',{
-        loginData: loginData
-    }).then(
-        res => {
-            console.log(res);
-            this.$router.push("/");
-        }
-    )
+const requestLogin = (loginID, loginPW, callback, errorCallback) => { // eslint-disable-line no-unused-vars
+	//백앤드와 로그인 통신하는 부분
+	let loginData = new Map();
+	loginData.set('email', loginID);
+	loginData.set('password', loginPW);
+	return axios.post('http://localhost:8080/user/login', loginData)
+		.then(
+			res => {
+				console.log(res);
+				this.$router.push("/");
+			}
+		)
 
 }
+export const requestRegister = (email, username, password, birth, gender) => {
+	let joinData = {
+		email: email,
+		name: username,
+		password: password,
+		birth: birth,
+		gender: gender
+	};
+	axios.post('http://localhost:8080/user/join', joinData)
+	.then(
+		res => {
+			console.log(res);
+			this.$router.push("/");
+		}
+	)
+};
 
 export const checkEmailExists = (email) => axios.get('/user/email/' + email);
-
-export const localRegister = ({email, username, password}) => axios.post('/user/join', { email, username, password });
-export const localLogin = ({email, password}) => axios.post('/user/login', { email, password });
 
 export const logout = () => axios.post('/api/auth/logout');
 
 
 const UserApi = {
-    requestLogin:(data,callback,errorCallback)=>requestLogin(data,callback,errorCallback)
+	requestLogin: (data, callback, errorCallback) => requestLogin(data, callback, errorCallback)
 }
 
 export default UserApi
