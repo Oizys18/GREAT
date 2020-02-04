dba532cef2c9e821ce38a99022dfe0073466d689
Oizys18 <oizys18@gmail.com>
2020-02-04 14:38:56 +0900

    Mod AppBar

--- front/src/App.vue
+++ front/src/App.vue
@@ -19,10 +19,11 @@ export default {
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Noto Sans KR', sans-serif;
+  /* font-family: "Avenir", Helvetica, Arial, sans-serif; */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  /* color: #2c3e50; */
 }
 </style>
