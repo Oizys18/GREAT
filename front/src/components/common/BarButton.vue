a59369e17586b2a21050ec786041acbede123af2
hanseul <hsdeb11@gmail.com>
2020-02-05 10:33:43 +0900

    Add BarButton.css hanseul

--- front/src/components/common/BarButton.vue
+++ front/src/components/common/BarButton.vue
@@ -6,50 +6,19 @@
   <span>Let's Start</span>
   <img 
       src="@/assets/img/arrow-icon.png"
-      class="btn-img"/>
+      class="bar-btn-img"/>
   </div>
 </div>
 </template>
 
 <script>
+import '@/assets/style/css/barButton.css'
 export default {
   name: "BarButton",
   methods: {
     go(link) {
       this.$router.push(link);
     }
-  },
-  data() {
-    return {
-      btnChange: false
-    }
   }
 };
 </script>
-
-<style>
-.bar-btn {
-  font-family: "Lobster", cursive;
-  background-color: #fbedeb;
-  padding: 3px 20px 3px 20px;
-  font-size: 1.2em;
-  cursor: pointer;
-  border-radius: 25px;
-  display: inline;
-  transition-duration: 0.5s;
-}
-.bar-btn img {
-  display: none;
-}
-.bar-btn:hover {
-  padding: 3px 40px 3px 20px;
-}
-.bar-btn:hover img {
-  display: inline;
-}
-.btn-img {
-  height: 90%;
-  position: absolute;
-  margin-top: 2px;
-}
-</style>
