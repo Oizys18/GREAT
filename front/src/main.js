a19a145bb53ddf8bdf201ed090e0ee6382587a6a
jisupark <seoyb105@naver.com>
2020-02-05 17:46:34 +0900

    Add social Login Kakao, Google & BackEnd Setting Jisu

--- front/src/main.js
+++ front/src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import router from './routes'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import GAuth from 'vue-google-oauth2'
 
+Vue.use(GAuth, {clientId: '962442152974-7o0e1a9e3pb31bpp51r039unqem2a2s0.apps.googleusercontent.com', scope: 'profile email https://www.googleapis.com/auth/plus.login'})
 Vue.config.productionTip = false
 
 new Vue({
