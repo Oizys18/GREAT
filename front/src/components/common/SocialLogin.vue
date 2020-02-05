e8203abbc8785431746fc0330b70cb0cb895b088
신채민 <chaem1996@gmail.com>
2020-02-05 17:49:28 +0900

    Mod Authentication.vue chaemin 회원가입 창에서 이메일 인증 처리 중

--- front/src/components/common/SocialLogin.vue
+++ front/src/components/common/SocialLogin.vue
@@ -1,18 +1,5 @@
 <template>
     <div class="social-login">
-        <div id="facebook-login">
-            <button>
-                <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" width="48" height="48" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                    viewBox="0 0 112.196 112.196" style="enable-background:new 0 0 112.196 112.196;" xml:space="preserve">
-                <g>
-                    <circle style="fill:#3B5998;" cx="56.098" cy="56.098" r="56.098"/>
-                    <path style="fill:#FFFFFF;" d="M70.201,58.294h-10.01v36.672H45.025V58.294h-7.213V45.406h7.213v-8.34
-                        c0-5.964,2.833-15.303,15.301-15.303L71.56,21.81v12.51h-8.151c-1.337,0-3.217,0.668-3.217,3.513v7.585h11.334L70.201,58.294z"/>
-                </g>
-                <!-- 아이콘 제작자 <a href="https://www.flaticon.com/kr/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/kr/" title="Flaticon"> www.flaticon.com</a> -->
-                </svg>
-            </button>
-        </div>
         <div id="kakao-login">
             <button>
                 <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 55 55">
