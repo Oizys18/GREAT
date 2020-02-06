0630f5d22c90b0ae762536e23b500c1e5a0ffe32
jisupark <seoyb105@naver.com>
2020-02-06 16:37:12 +0900

    Add map jisu Add map feature

---
+++ front/src/views/Map.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <MapApp/>
+  </div>
+</template>
+ <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=f8d38a34b065785c71e6beed1528657f"></script>
+<script>
+import MapApp from "@/components/common/MapApp.vue"
+export default {
+  name: "Index",
+  components: {
+    MapApp
+  },
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
