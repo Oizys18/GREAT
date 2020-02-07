3529f40d64531ab0de8172cf5c4cebbb7ead2705
조한슬 <hsdeb11@gmail.com>
2020-02-07 02:28:54 +0000

    Fe sidebar

---
+++ front/src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="sidebar-container">
+    <!-- <div class="sidebar-image">aa</div> -->
+    <StoreImage :url="storeInfo.image"/>
+  </div>
+</template>
+
+<script>
+import '../../assets/style/css/Sidebar.css'
+import StoreImage from '../Sidebar/StoreImage'
+import GridApi from '../../apis/GridApi.js'
+export default {
+  name: 'Sidebar',
+  props: ["id"],
+  components: {
+    StoreImage 
+  },
+  mounted() {
+    GridApi.requestStoreInfo(this.id, response => {
+      this.$store.state.storeInfo = response
+    })
+  },
+  computed: {
+    storeInfo() {
+      return this.$store.state.storeInfo
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
