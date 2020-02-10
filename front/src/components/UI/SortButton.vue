a3afd143e18e17c88e09aa33243ece552b2c7664
jisupark <seoyb105@naver.com>
2020-02-10 09:16:04 +0900

    Add sortButton jisu

--- front/src/components/UI/SortButton.vue
+++ front/src/components/UI/SortButton.vue
@@ -1,22 +1,80 @@
 <template>
-  <div class="sort-elements"> 
-  <div class="btn-group">
-    <button type="button" class="btn btn-primary sort rating">
-      평점순
-    </button>
+  <div class="sort-elements">
+    <div class="btn-group">
+      <button
+        @click="orderByRating"
+        id="rating"
+        type="button"
+        class="btn btn-primary sort rating"
+      >평점순</button>
 
-    <button type="button" class="btn btn-primary sort distance">
-      거리순
-    </button>
+      <button
+        @click="orderByDistance"
+        id="distance"
+        type="button"
+        class="btn btn-primary sort distance"
+      >거리순</button>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
-import "@/assets/style/css/sortButton.css"
+import "@/assets/style/css/sortButton.css";
+import GridApi from "../../apis/GridApi.js";
 export default {
-  name:"SortButton"
-}
+  name: "SortButton",
+  data() {
+    return {
+      order: ""
+    };
+  },
+  methods: {
+    orderByRating() {
+      if (this.order != "rating") {
+        this.order = "rating";
+        console.log(this.order);
+        var categories = this.$store.state.categories;
+        for (var i = 0; i < categories.length; i++) {
+          var categoryName = categories[i].name;
+          var categoryId = categories[i].id;
+          var data = {
+            category: categoryId
+          };
+          GridApi.requestGridStoresByRating(data, response => {
+            for (var i = response.length; i < 8; i++) {
+                response.push({ name: "" });
+            }
+            this.$store.commit(categoryName, response);
+          });
+        }
+      }
+    },
+    orderByDistance() {
+      if (this.order != "distance") {
+        this.order = "distance";
+        console.log(this.order);
+        var categories = this.$store.state.categories;
+        for (var i = 0; i < categories.length; i++) {
+          var categoryName = categories[i].name;
+          var categoryId = categories[i].id;
+          var x = this.$store.state.locationX;
+          var y = this.$store.state.locationY;
+          var data = {
+            locationX: x,
+            locationY: y,
+            category: categoryId
+          };
+          GridApi.requestGridStores(data, response => {
+            for (var i = response.length; i < 8; i++) {
+                response.push({ name: "" });
+            }
+            this.$store.commit(categoryName, response);
+          });
+        }
+      }
+    }
+  }
+};
 </script>
 
 <style></style>
