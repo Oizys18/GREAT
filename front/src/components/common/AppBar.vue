08b01a55b9843559aecf395ef5d121859c2392ef
Oizys18 <oizys18@gmail.com>
2020-02-11 13:30:49 +0900

    Mod appbar chanu mod appbar - transparent

--- front/src/components/common/AppBar.vue
+++ front/src/components/common/AppBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app-bar" class="animated fadeInDown delay-0.2s">
-    <div class="bar-banner" @click="go('/')">GrEAT</div>
-    <div class="bar-router">
+    <div class="app-bar-banner" @click="go('/')">GrEAT</div>
+    <div class="app-bar-router">
       <BarButton />
     </div>
   </div>
@@ -29,19 +29,22 @@ export default {
   z-index: 100;
   position: fixed;
   width: 100%;
-  background: linear-gradient(72deg, #c0392b, #f1c40f);
+  /* background: linear-gradient(72deg, #c0392b, #f1c40f); */
+  background: transparent;
   padding: 15px;
   font-size: 1.2em;
   display: flex;
 }
-.bar-banner {
+.app-bar-banner {
   font-family: "Lobster", cursive;
   color: rgb(236, 236, 236);
   position: relative;
   cursor: pointer;
+  font-size:2rem;
+  text-shadow:0px 2px 2px black;
   left: 0;
 }
-.bar-router {
+.app-bar-router {
   position: fixed;
   right: 0;
   padding: 0 10px 0 0;
