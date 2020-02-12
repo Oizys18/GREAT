486f30a8ab9cdb768adc41b89246f08b0ab685c1
sinbicho <tjsb0108@naver.com>
2020-02-12 17:49:18 +0900

    Mod FoodGridList Add StoreInfoModal.vue

--- front/src/apis/UserApi.js
+++ front/src/apis/UserApi.js
@@ -41,7 +41,6 @@ const requestToken = () => {
 
 const requestLogin = (remID,loginID, loginPW, callback, errorCallback) => { // eslint-disable-line no-unused-vars
 	storage.setItem('email',loginID);
-	// else session.setItem('email',loginID);
 	return axios.post('http://13.124.1.176:8080/user/login', {
 			email: loginID,
 			password: loginPW
@@ -56,7 +55,7 @@ const requestLogin = (remID,loginID, loginPW, callback, errorCallback) => { // e
 
 const requestLogout = () => {
 	storage.setItem('id',null);
-	storage.setItem('email',null); //신비 추가 
+	storage.setItem('email',null); 
 	storage.setItem('token', null);
 	return storage.getItem('token');
 };
