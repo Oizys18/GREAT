9ba7ee0fb8f729044969ecf101119ab434d1514f
jisupark <seoyb105@naver.com>
2020-02-14 11:06:39 +0900

    Update sortBtn design jisu

--- front/src/components/UI/SortButton.vue
+++ front/src/components/UI/SortButton.vue
@@ -1,20 +1,23 @@
 <template>
+
   <ul class="sort-elements">
+    <div class="sort-wrapper">
     <li>
       <a href="#" @click="click('rating')">
         <span class="icon-cog"></span>
       </a>
-    </li>
+    </li><br>
     <li>
       <a href="#" @click="click('random')">
         <span class="icon-cw"></span>
       </a>
-    </li>
+    </li><br>
     <li>
       <a href="#" @click="click('distance')">
         <span class="icon-location"></span>
       </a>
     </li>
+    </div>
   </ul>
 </template>
 
@@ -23,15 +26,22 @@ import "@/assets/style/css/sortButton.css";
 import GridApi from '../../apis/GridApi.js'
 export default {
   name: "SortButton",
+  data(){
+    return{
+      sort : ""
+    };
+  },
   methods: {
     click(type) {
-      var x = this.$store.state.locationX;
-      var y = this.$store.state.locationY;
-      var categories = this.$store.state.categories;
-      for (var i = 0; i < categories.length; i++) {
-        var categoryName = categories[i].name;
-        var categoryId = categories[i].id;
-        this.apiCall(categoryId, categoryName, x, y, type);
+      if(type!=this.sort){
+        var x = this.$store.state.locationX;
+        var y = this.$store.state.locationY;
+        var categories = this.$store.state.categories;
+        for (var i = 0; i < categories.length; i++) {
+          var categoryName = categories[i].name;
+          var categoryId = categories[i].id;
+          this.apiCall(categoryId, categoryName, x, y, type);
+        }
       }
     },
     apiCall(categoryId, categoryName, x, y, type) {
@@ -43,6 +53,7 @@ export default {
 
       switch (type) {
         case "rating":
+          this.sort="rating"
           GridApi.requestGridStoresByRating(data, response => {
             for (var i = response.length; i < 8; i++) {
               response.push({ name: "" });
@@ -59,6 +70,7 @@ export default {
           });
           break;
         case "distance":
+          this.sort="distance"
           GridApi.requestGridStoresByDistance(data, response => {
             for (var i = response.length; i < 8; i++) {
               response.push({ name: "" });
