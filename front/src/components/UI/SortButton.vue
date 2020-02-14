f33341532cd9d2369c42234d98c796ffbe3c4249
jisupark <seoyb105@naver.com>
2020-02-14 14:14:46 +0900

    Update SortButton Design & function jisu

--- front/src/components/UI/SortButton.vue
+++ front/src/components/UI/SortButton.vue
@@ -3,18 +3,27 @@
   <ul class="sort-elements">
     <div class="sort-wrapper">
     <li>
-      <a href="#" @click="click('rating')">
+      <a href="#" @click="click('rating')" class="hello">
         <span class="icon-cog"></span>
+        <label>별점순</label>
       </a>
     </li><br>
     <li>
       <a href="#" @click="click('random')">
         <span class="icon-cw"></span>
+        <label>랜덤순</label>
       </a>
     </li><br>
     <li>
       <a href="#" @click="click('distance')">
         <span class="icon-location"></span>
+        <label>거리순</label>
+      </a>
+    </li><br>
+    <li>
+      <a href="#" @click="click('bookmark')">
+        <span class="icon-bookmark"></span>
+        <label>북마크</label>
       </a>
     </li>
     </div>
@@ -24,6 +33,7 @@
 <script>
 import "@/assets/style/css/sortButton.css";
 import GridApi from '../../apis/GridApi.js'
+import BookmarkApi from '../../apis/BookmarkApi.js'
 export default {
   name: "SortButton",
   data(){
@@ -33,7 +43,12 @@ export default {
   },
   methods: {
     click(type) {
-      if(type!=this.sort){
+      if(type == "bookmark"){
+       var input =  prompt("북마크 이름을 설정해 주세요.");
+       if(input!=null)
+        this.gridBookmark(input);
+      }
+      else if(type!=this.sort){
         var x = this.$store.state.locationX;
         var y = this.$store.state.locationY;
         var categories = this.$store.state.categories;
@@ -79,6 +94,24 @@ export default {
           });
           break;
       }
+    },
+        gridBookmark(input) {
+      this.$store.commit('setGridBookmarkList')
+
+      var data = {
+        'name': input,
+        'stores': this.$store.state.gridBookmarkStoreList,
+        'type': 'G',
+        'user': localStorage.getItem('id')
+      }
+
+      BookmarkApi.requestGridBookmarkPost(data, response => {
+        if(response == 'success') {
+          alert('그리드 북마크를 등록했습니다.')
+        }
+      }, () => { 
+        alert('그리드 북마크는 로그인 후 가능합니다.')
+      })
     }
   }
 };
