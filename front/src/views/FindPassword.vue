6e13ef0f9778320a8125e303804fd2c1681391b1
신채민 <chaem1996@gmail.com>
2020-02-17 16:52:09 +0900

    Mod FindPassword.vue chaemin find password page complete

--- front/src/views/FindPassword.vue
+++ front/src/views/FindPassword.vue
@@ -6,14 +6,10 @@
         <div class="input-with-label email-input">
           <input v-model="email" id="email" placeholder="이메일을 입력하세요." type="text" />
           <label for="email">이메일</label>
-          <button
-            class="v-btn v-btn--contained v-btn--rounded theme--dark"
-            id="email-check"
-            @click="emailCheck()"
-            >전송</button>
+          <v-btn style="margin-top: 5px" rounded color="#ff7761" dark @click="sendPW()">전송</v-btn>
         </div>
-        <v-btn rounded color="#ff7761" dark @click="socialjoinApi()">로그인하기</v-btn>
       </div>
+      <v-btn style="margin: 0 auto; margin-top: 10px; width: 50%;" color="#ff7761" dark @click="redirectLogin()">로그인 화면으로</v-btn>
     </div>
   </div>
 </template>
@@ -26,13 +22,21 @@ import UserApi from "@/apis/UserApi";
 
 export default {
   created() {
+    this.email = "";
     this.sns_token = sessionStorage.getItem("sns_token");
   },
   methods: {
+    sendPW(){
+      let email = this.email;
+      
+    },
+    redirectLogin(){
+      this.$router.push("Authentication");
+    }
   },
   data() {
     return {
-      
+      email: "",
       component: this
     };
   }
