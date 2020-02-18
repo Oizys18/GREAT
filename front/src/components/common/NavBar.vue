3a188167036bf49a26189697f71b993db7e91557
Oizys18 <oizys18@gmail.com>
2020-02-18 11:26:01 +0900

    Add navbar style chanu

--- front/src/components/common/NavBar.vue
+++ front/src/components/common/NavBar.vue
@@ -2,10 +2,31 @@
   <div class="nav-bar animated fadeInDown delay-0.2s" id="navbar">
     <div class="nav-bar-banner-container">
       <div class="nav-bar-mainbanner" id="navbarMB" @click="go('/')">
-        <img class="bt1" src="https://i.imgur.com/si3Uh5E.png" alt="GR" />
-        <img class="bt2" src="https://i.imgur.com/enHnxlO.png" alt="EAT" />
-        <img class="bt3" src="https://i.imgur.com/GvcutCs.png" alt="그래" />
-        <img class="bt4" src="https://i.imgur.com/GVGfFoN.png" alt="잇" />
+        <img
+          id="bt1"
+          class="bt1"
+          src="https://i.imgur.com/si3Uh5E.png"
+          alt="GR"
+        />
+        <img
+          id="bt2"
+          class="bt2"
+          src="https://i.imgur.com/enHnxlO.png"
+          alt="EAT"
+        />
+        <!-- {{scrollPosition}} -->
+        <img
+          id="bt3"
+          class="bt3"
+          src="https://i.imgur.com/GvcutCs.png"
+          alt="그래"
+        />
+        <img
+          id="bt4"
+          class="bt4"
+          src="https://i.imgur.com/GVGfFoN.png"
+          alt="잇"
+        />
       </div>
     </div>
     <div class="nav-bar-router">
@@ -45,20 +66,58 @@ export default {
   },
   watch: {
     scrollPosition() {
-      if (this.scrollPosition <= 90) {
-        var ab1 = document.getElementById("navbar");
-        ab1.style.background = "transparent";
+      if (this.scrollPosition <= 50) {
+        var b1 = document.getElementById("bt1");
+        var b2 = document.getElementById("bt2");
+        var b3 = document.getElementById("bt3");
+        var b4 = document.getElementById("bt4");
+        // b2
+        b2.style.webkitTransform =
+          "translate(" +
+          b1.offsetWidth / 2 +
+          "px" +
+          "," +
+          b1.offsetHeight +
+          "px)";
 
-        var bt1 = document.getElementById("navbarMB").classList;
-        bt1.remove("banner-text-transition");
+        // b3
+        b3.style.webkitTransform =
+          "translate(" + (5 + b1.offsetWidth) + "px" + "," + 0 + "px)";
+
+        // // b4
+        b4.style.webkitTransform =
+          "translate(" +
+          (b1.offsetWidth / 2 + b2.offsetWidth) +
+          "px" +
+          "," +
+          b2.offsetHeight +
+          "px)";
+
       } else {
-        var ab2 = document.getElementById("navbar");
-        var idxB = document.getElementById("index-background").style
-          .backgroundColor;
-        ab2.style.background = idxB;
+        var bt1 = document.getElementById("bt1");
+        var bt2 = document.getElementById("bt2");
+        var bt3 = document.getElementById("bt3");
+        var bt4 = document.getElementById("bt4");
+        bt2.style.webkitTransform =
+          "translate(" +
+          bt1.offsetWidth+
+          "px" +
+          "," +
+          0 +
+          "px)";
 
-        var bt2 = document.getElementById("navbarMB").classList;
-        bt2.add("banner-text-transition");
+        // b3
+        bt3.style.webkitTransform =
+          "translate(" + (bt2.offsetWidth + bt1.offsetWidth) + "px" + "," + 0 + "px)";
+
+        // // b4
+        bt4.style.webkitTransform =
+          "translate(" +
+          (bt1.offsetWidth + bt2.offsetWidth + bt3.offsetWidth) +
+          "px" +
+          "," +
+          0 +
+          "px)";
       }
     }
   }
